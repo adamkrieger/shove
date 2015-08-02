015a4b78c0940bba80f3f0c35e6e4c0a9d22cbe6
adamkrieger <adamkrieger.ca@gmail.com>
2015-08-01 22:14:06 -0500

    Moved s3 functions to their own package. Added content type determination for xml, html, and css.

---
+++ s3mgmt/s3mgmt.go
@@ -0,0 +1,68 @@
+package s3mgmt
+
+import (
+  "github.com/aws/aws-sdk-go/aws"
+  "github.com/aws/aws-sdk-go/aws/awserr"
+  "github.com/aws/aws-sdk-go/aws/awsutil"
+  "github.com/aws/aws-sdk-go/service/s3"
+  "fmt"
+)
+
+func ListBuckets(region string) {
+  config := aws.NewConfig().WithRegion(region)
+  svc := s3.New(config)
+
+  var params *s3.ListBucketsInput
+  resp, err := svc.ListBuckets(params)
+
+  if err != nil {
+  	if awsErr, ok := err.(awserr.Error); ok {
+  		// Generic AWS error with Code, Message, and original error (if any)
+  		fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
+  		if reqErr, ok := err.(awserr.RequestFailure); ok {
+  			// A service error occurred
+  			fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
+  		}
+  	} else {
+  		// This case should never be hit, the SDK should always return an
+  		// error which satisfies the awserr.Error interface.
+  		fmt.Println(err.Error())
+  	}
+  }
+
+  // Pretty-print the response data.
+  fmt.Println(awsutil.Prettify(resp))
+}
+
+func ListBucketContents(region, bucket string) {
+  config := aws.NewConfig().WithRegion(region)
+  svc := s3.New(config)
+
+  params := &s3.ListObjectsInput{
+  	Bucket:       aws.String(bucket), // Required
+  	// Delimiter:    aws.String("Delimiter"),
+  	// EncodingType: aws.String("EncodingType"),
+  	// Marker:       aws.String("Marker"),
+  	// MaxKeys:      aws.Int64(1),
+  	// Prefix:       aws.String("Prefix"),
+  }
+  resp, err := svc.ListObjects(params)
+
+  if err != nil {
+  	if awsErr, ok := err.(awserr.Error); ok {
+  		// Generic AWS error with Code, Message, and original error (if any)
+  		fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
+  		if reqErr, ok := err.(awserr.RequestFailure); ok {
+  			// A service error occurred
+  			fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
+  		}
+  	} else {
+  		// This case should never be hit, the SDK should always return an
+  		// error which satisfies the awserr.Error interface.
+  		fmt.Println(err.Error())
+  	}
+  }
+
+  // Pretty-print the response data.
+  fmt.Println(awsutil.Prettify(resp))
+}
--- shove.go
+++ shove.go
@@ -2,74 +2,25 @@ package main
 
 import (
   "github.com/aws/aws-sdk-go/aws"
-  "github.com/aws/aws-sdk-go/aws/awserr"
-  "github.com/aws/aws-sdk-go/aws/awsutil"
-  "github.com/aws/aws-sdk-go/service/s3"
   "github.com/aws/aws-sdk-go/service/s3/s3manager"
   "github.com/codegangsta/cli"
   "os"
   "log"
   "path/filepath"
   "fmt"
+  "./s3mgmt"
 )
 
-func listobjects(region, bucket string) {
-  config := aws.NewConfig().WithRegion(region)
-  svc := s3.New(config)
-
-  params := &s3.ListObjectsInput{
-  	Bucket:       aws.String(bucket), // Required
-  	// Delimiter:    aws.String("Delimiter"),
-  	// EncodingType: aws.String("EncodingType"),
-  	// Marker:       aws.String("Marker"),
-  	// MaxKeys:      aws.Int64(1),
-  	// Prefix:       aws.String("Prefix"),
+func getContentType(extension string) string {
+  switch extension {
+  case ".html":
+    return "text/html"
+  case ".xml":
+    return "text/xml"
+  case ".css":
+    return "text/css"
   }
-  resp, err := svc.ListObjects(params)
-
-  if err != nil {
-  	if awsErr, ok := err.(awserr.Error); ok {
-  		// Generic AWS error with Code, Message, and original error (if any)
-  		fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
-  		if reqErr, ok := err.(awserr.RequestFailure); ok {
-  			// A service error occurred
-  			fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
-  		}
-  	} else {
-  		// This case should never be hit, the SDK should always return an
-  		// error which satisfies the awserr.Error interface.
-  		fmt.Println(err.Error())
-  	}
-  }
-
-  // Pretty-print the response data.
-  fmt.Println(awsutil.Prettify(resp))
-}
-
-func list(region string) {
-  config := aws.NewConfig().WithRegion(region)
-  svc := s3.New(config)
-
-  var params *s3.ListBucketsInput
-  resp, err := svc.ListBuckets(params)
-
-  if err != nil {
-  	if awsErr, ok := err.(awserr.Error); ok {
-  		// Generic AWS error with Code, Message, and original error (if any)
-  		fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
-  		if reqErr, ok := err.(awserr.RequestFailure); ok {
-  			// A service error occurred
-  			fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
-  		}
-  	} else {
-  		// This case should never be hit, the SDK should always return an
-  		// error which satisfies the awserr.Error interface.
-  		fmt.Println(err.Error())
-  	}
-  }
-
-  // Pretty-print the response data.
-  fmt.Println(awsutil.Prettify(resp))
+  return "binary/octet-stream"
 }
 
 type fileWalk chan string
@@ -111,14 +62,18 @@ func upload(region, bucket, directory string) {
           continue
       }
       defer file.Close()
+      contentType := getContentType(filepath.Ext(path))
       _, err = uploader.Upload(&s3manager.UploadInput{
           Bucket: &bucket,
           Key:    aws.String(filepath.Join(prefix, rel)),
           Body:   file,
+          ContentType: &contentType,
       })
       if err != nil {
           log.Fatalln("Failed to upload", path, err)
       }
+
+      // fmt.Println(prefix, rel, filepath.Ext(path), getContentType(filepath.Ext(path)))
   }
 }
 
@@ -139,7 +94,7 @@ func main() {
       Aliases: []string{"l"},
       Usage: "List available buckets.",
       Action: func(c *cli.Context) {
-        list(c.GlobalString("region"))
+        s3mgmt.ListBuckets(c.GlobalString("region"))
       },
     },
     {
@@ -153,7 +108,7 @@ func main() {
         },
       },
       Action: func(c *cli.Context) {
-        listobjects(c.GlobalString("region"), c.String("bucket"))
+        s3mgmt.ListBucketContents(c.GlobalString("region"), c.String("bucket"))
       },
     },
     {
